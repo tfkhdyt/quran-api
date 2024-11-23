c66d2ebbd4bc9273738ac83c7a35f58a7d8315c9
Taufik Hidayat <tfkhdyt@proton.me>
2024-11-23 16:09:47 +0700

    feat(api): add route to get a single surah

--- src/surah.ts
+++ src/surah.ts
@@ -4,12 +4,13 @@ import quranJson from '../data/quran.json' assert { type: 'json' };
 import { cache } from './cache/lru.js';
 import { cacheMiddleware } from './cache/middleware.js';
 
-const quranData = quranJson as Quran;
+const { data: quranData } = quranJson as Quran;
 
 const app = new Hono();
 
+// get all surah
 app.get('/', cacheMiddleware, (c) => {
-  const data = quranData.data.map((item) => {
+  const data = quranData.map((item) => {
     const surah = { ...item };
     delete surah.verses;
     delete surah.preBismillah;
@@ -22,8 +23,29 @@ app.get('/', cacheMiddleware, (c) => {
     message: 'Success fetching all surah',
     data,
   };
+  cache.set(c.req.path, response);
 
-  cache.set(c.req.url, response);
+  return c.json(response);
+});
+
+// get a surah
+app.get('/:id', cacheMiddleware, async (c) => {
+  const id = Number(c.req.param('id'));
+
+  const data = quranData.at(id - 1);
+  if (!data) {
+    return c.json({
+      success: false,
+      message: `Surah "${id}" is not found`,
+    });
+  }
+
+  const response = {
+    success: true,
+    message: 'Success fetching surah',
+    data,
+  };
+  cache.set(c.req.path, response);
 
   return c.json(response);
 });
